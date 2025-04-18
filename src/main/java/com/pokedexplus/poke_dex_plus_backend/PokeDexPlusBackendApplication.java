0cb60275ac8091f61a134ee896db58dc0c2ea139
Omar Aguirre <omar.fi.wwr@gmail.com>
2025-04-17 22:07:18 -0600

    Base spring project created

---
+++ src/main/java/com/pokedexplus/poke_dex_plus_backend/PokeDexPlusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pokedexplus.poke_dex_plus_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeDexPlusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeDexPlusBackendApplication.class, args);
+	}
+
+}
