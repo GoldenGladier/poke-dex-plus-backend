e83be21366b156aac574e91f44720de1c0ae5d28
Omar Aguirre <omar.fi.wwr@gmail.com>
2025-04-19 13:30:44 -0600

    faeture - Adding Cors configuration to allow access from localhost to API (for development)

--- src/main/java/com/pokedexplus/poke_dex_plus_backend/configuration/WebConfig.java
+++ src/main/java/com/pokedexplus/poke_dex_plus_backend/configuration/WebConfig.java
@@ -3,6 +3,7 @@ package com.pokedexplus.poke_dex_plus_backend.configuration;
 import com.pokedexplus.poke_dex_plus_backend.configuration.interceptor.ApiAccessInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,5 +16,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(apiAccessInterceptor)
                 .addPathPatterns("/pokedex/**"); // For all: "/**"
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:*")  // Only for development
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
